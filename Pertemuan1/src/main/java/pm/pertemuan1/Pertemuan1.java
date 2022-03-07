13e8a9c9a154a457660858edb883ac1aa96103db
irwan_maulana42 <irwanmaulana@prisma-ads.com>
2022-03-07 16:04:22 +0700

    UPDATE

--- Pertemuan1/src/main/java/pm/pertemuan1/Pertemuan1.java
+++ Pertemuan1/src/main/java/pm/pertemuan1/Pertemuan1.java
@@ -5,8 +5,6 @@
 package pm.pertemuan1;
 
 import java.io.IOException;
-import java.io.Console;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,22 +16,33 @@ public class Pertemuan1 {
 
     public static void main(String[] args) {
 //        Class Scanner
-//        InputWithScanner withScanner = new InputWithScanner();
-//        withScanner.scanner();
-
+        System.out.println("CLASS SCANNER");
+        System.out.println("===================================");
+        InputWithScanner withScanner = new InputWithScanner();
+        withScanner.scanner();
+        System.out.println("===================================");
+        
 //        Class BufferedReader
-//        InputWithBufferReader withBuffered = new InputWithBufferReader();
-//        try {
-//            withBuffered.bufferReader();
-//        } catch (IOException ex) {
-//            System.out.println("Message " + ex);
-//        }
+        System.out.println("CLASS BUFFERED READER");
+        System.out.println("===================================");
+        InputWithBufferReader withBuffered = new InputWithBufferReader();
+        try {
+            withBuffered.bufferReader();
+        } catch (IOException ex) {
+            System.out.println("Message " + ex);
+        }
+        System.out.println("===================================");
+        
+        
 //        Class Console
+        System.out.println("CLASS CONSOLE");
+        System.out.println("===================================");
         InputWithConsole withConsole = new InputWithConsole();
         try {
             withConsole.console();
         } catch (IOException ex) {
-            Logger.getLogger(Pertemuan1.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Message " + ex);
         }
+        System.out.println("===================================");
     }
 }
